6e6c6b000dafd88a594e864f71dc21ba587ad019
pedropbr <pedropbr@gmail.com>
2016-06-27 12:58:10 +0000

    Automated Rebuild

---
+++ automated_build.sh
@@ -0,0 +1,23 @@
+# Copy static site
+CWD=`pwd`
+
+# Clone Pages repo
+cd /tmp
+git clone git@github.com:pedropbr/laughing-guacamole build
+cd build && git checkout -b gh-pages origin/gh-pages
+
+# Trigger Jekyll rebuild
+cd $CWD
+bundle exec jekyll contentful --rebuild
+
+# Push newly built repo
+cp -r $CWD/_site/* /tmp/build
+
+cd /tmp/build
+
+git config --global user.email "pedropbr@gmail.com"
+git config --global user.name "pedropbr"
+
+git add .
+git commit -m "Automated Rebuild"
+git push -f origin gh-pages
