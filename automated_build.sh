dc843202fab4b79a2c77e55e3a8a148d092484b7
Pedro Rebucci <pedro@contentful.com>
2016-06-27 10:10:37 +0200

    automation

--- automated_build.sh
+++ automated_build.sh
@@ -3,7 +3,7 @@ CWD=`pwd`
 
 # Clone Pages repo
 cd /tmp
-git clone master build
+git clone gh-pages build
 # cd build && git checkout -b YOUR_BRANCH origin/YOUR_BRANCH # If not using master
 
 # Trigger Jekyll rebuild
